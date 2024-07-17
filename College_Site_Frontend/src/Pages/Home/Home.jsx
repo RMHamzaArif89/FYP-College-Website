c51f500fbd1552deb2d4b6ee7b2445496ec04e17
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-17 07:47:56 +0500

    created eventData for cardSlider...

--- College_Site_Frontend/src/Pages/Home/Home.jsx
+++ College_Site_Frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ImgSlider from '../../Components/ImgSlider/ImgSlider'
+import CardSlider from '../../Components/CardSlider/CardSlider'
 
 function Home() {
   return (
     <div className='home'>
       <ImgSlider/>
+      <CardSlider/>
     </div>
   )
 }
