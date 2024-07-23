451465fc993f48c74cd05a35386a2516ea488089
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-23 09:38:18 +0500

    added img to quickFacts page...

--- College_Site_Frontend/src/Pages/QuickFacts/QuickFacts.jsx
+++ College_Site_Frontend/src/Pages/QuickFacts/QuickFacts.jsx
@@ -13,13 +13,16 @@ import { QFData } from './QFData';
 function QuickFacts() {
   return (
     <div className='quickFacts'>
-     <h1 className="eventHeading">
-        Events
+     <h1 className="quickFactsHeading">
+        Quick Facts
       </h1>
+      <div className="quickFactsDescription">
+      At GGCSF we are proud of our accomplishments. Together, faculty, students, alumni and staff contribute to creating an amazing experience. Here are just a few reasons why GGCSF is the top university in Pakistan
+      </div>
       <Swiper
         effect={'coverflow'}
         autoplay={{
-          delay: 1500,
+          delay: 2000,
           pauseOnMouseEnter: true,
           disableOnInteraction: false
           // disableOnInteraction: false
@@ -41,28 +44,25 @@ function QuickFacts() {
           clickable: true,
         }}
         modules={[Autoplay,EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
+        className="quickFacts_swiper_container quickFactsSwiperCon"
       >
       {
         QFData.map((facts)=>{
           return(
-            <SwiperSlide className='swiper-slide'>
-              <img src="" alt="" className='eventImg'/>
-             <div className="eventText">
+            <SwiperSlide className='quickFacts_swiper_slide quickFactsSwiperSlide'>
+              
+             
              <div className="eventTitle">{facts.name}</div>
               <div className="eventDate">{facts.value}</div>
              
-             </div>
+             
 
             </SwiperSlide>
           )
         })
       }
 
-        <div className="slider-controler">
-          
-          <div className="swiper-pagination"></div>
-        </div>
+       
       </Swiper>
     </div>
   )
