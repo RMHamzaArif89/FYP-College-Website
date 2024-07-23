194ad0a0834c3494a5e90b8969e2e0a0a6ea680d
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-23 18:24:11 +0500

    created sports page...

--- College_Site_Frontend/src/Pages/Sports/Sports.jsx
+++ College_Site_Frontend/src/Pages/Sports/Sports.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cube';
 import './sports.css'
 function Sports() {
   return (
-    <div>
-      
+    <div className='sports'>
+      <div className="sportsHeading">Sport and Studies go together</div>
+      <div className="sportsHeading2">Different Sports Event held for </div>
+
+      <Swiper
+        effect={'cube'}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
+      >
+  
+      </Swiper>
     </div>
   )
 }
