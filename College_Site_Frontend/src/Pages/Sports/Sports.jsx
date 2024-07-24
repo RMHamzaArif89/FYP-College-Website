ceb660032654c81de2f70016f716daaf439b1960
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-24 19:38:03 +0500

    created sports rows and video section...

--- College_Site_Frontend/src/Pages/Sports/Sports.jsx
+++ College_Site_Frontend/src/Pages/Sports/Sports.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import { SportsCubeData } from './sportsData';
+import { SportsCubeData, sportsNews } from './sportsData';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import {Autoplay, Pagination , EffectCube} from 'swiper/modules'
 import 'swiper/css';
 import 'swiper/css/effect-cube';
 import './sports.css'
+
 function Sports() {
   return (
     <div className='sports'>
+      <div className="sportsCon">
       <div className="sportsHeading">Sport and Studies go together</div>
       <div className="sportsHeading2">Different Sports Event held for </div>
 
       <Swiper
         effect={'cube'}
         grabCursor={true}
+        autoplay={{
+          delay: 2000,
+          pauseOnMouseEnter: true,
+          disableOnInteraction: false
+          // disableOnInteraction: false
+      }}
+        loop={true}
         cubeEffect={{
           shadow: true,
           slideShadows: true,
@@ -20,13 +30,13 @@ function Sports() {
           shadowScale: 0.94,
         }}
         pagination={true}
-        modules={[EffectCube, Pagination]}
-        className="mySwiper"
+        modules={[Autoplay, EffectCube, Pagination]}
+        className="sportsSwiper"
       >
         {
           SportsCubeData.map((data)=>{
             return(
-              <SwiperSlide className='cubeImg' style={{backgroundImage:`${data.img}`}}>
+              <SwiperSlide className='sportsSwiperSlide' style={{backgroundImage:`url(${data.img})`}}>
 
                 <div className="cubeHeading">{data.heading}</div>
                 <div className="cubeText">{data.text}</div>
@@ -36,7 +46,31 @@ function Sports() {
         }
   
       </Swiper>
+      </div>
+
+      <div className="sportsCon2">
+        <div className="sportsCol1">
+          {
+            sportsNews.map((data)=>{
+              return(
+                <div className="sportsCol1Card">
+                <img src={data.img} alt="" className="sportsCol1CardImg" />
+                <div className="sportsCol1Text">
+                  <div className="sportsCol1Title">{data.heading}</div>
+                  <div className="sportsCol1Date">{data.date}</div>
+                  <div className="sportsCol1Detail">{data.text}</div>
+                </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className="sportsCol2">
+        <video src="/video/homepagevideo.mp4" controls muted autoplay></video>
+        </div>
+      </div>
     </div>
+
   )
 }
 
--- College_Site_Frontend/src/Pages/Sports/sportsData.js
+++ College_Site_Frontend/src/Pages/Sports/sportsData.js
@@ -20,3 +20,30 @@ export const SportsCubeData=[
         img:'/images/logo.jpg'
     },
 ]
+
+export const sportsNews=[
+    {
+        img:'/images/imgSlider1.jpg',
+        heading:'Cricket Tournament District level',
+        date:'22-2-24',
+        text:'detai deatil kd kd kd '
+    },
+    {
+        img:'/images/imgSlider2.jpg',
+        heading:'Foot tournament',
+        date:'22-3-24',
+        text:'fjkdjd kdj kjdkfj '
+    },
+    {
+        img:'',
+        heading:'',
+        date:'',
+        text:''
+    },
+    {
+        img:'',
+        heading:'',
+        date:'',
+        text:''
+    },
+]
