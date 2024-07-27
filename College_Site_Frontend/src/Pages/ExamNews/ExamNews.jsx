f7c2fa8ab66b42aa7ff3cbbeae6082fa955f75b9
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-27 20:45:52 +0500

    created exam news page...

---
+++ College_Site_Frontend/src/Pages/ExamNews/ExamNews.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './examNews.css'
+
+function ExamNews() {
+  return (
+    <div className='examNews'>
+      <div className="examNewsCon">
+        
+      </div>
+    </div>
+  )
+}
+
+export default ExamNews
