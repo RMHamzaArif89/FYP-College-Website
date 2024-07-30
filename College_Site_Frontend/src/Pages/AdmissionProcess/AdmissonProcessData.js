5c3dea0b912abe48347196dd306d3752ec8ca5cd
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-07-30 20:28:26 +0500

    created the AdmissionProcessData in js file..

---
+++ College_Site_Frontend/src/Pages/AdmissionProcess/AdmissonProcessData.js
@@ -0,0 +1,131 @@
+export const APData=[
+    {
+        heading:'Admission Criteria',
+        text:'Academic Requiments: A candidate seeking admission to the degreee of BS (4years) must have passed FA/FSc/ICS/A level or equivalent (to e determined by the Equivalence Committee) from a recognized instituion with at least 45% marks or 2nd division on the basis of the respective category of admission, excluding 20 marks each of NCC and Hafiz-e-Quran.',
+        Links:[
+            'Domicile: The candidate should submit the domicile of Punjab Province. The candidate with domicile other than the Punjab province has to produce a nomination letter form his/her respective provinicuial governmetn againstt reserved seats reserved seats if any. Foreign studewnts can also apply through the relavant Federal Ministry.'
+        ]
+
+    },
+    {
+        heading:'Medium of Instruction',
+        text:'The medium of instrucion and examination shall be English (except faculty of islanic and Oriental Learning, which will be Urdu or thier relevant subect languages.)'
+    },
+    {
+        heading:'Duration of the course',
+        text:'The duration of the instruciton and examination shall be English (except faculty of islamic and Oriental Learning, which will be Urdu or thier relevant subject languages).'
+    },
+    {
+        heading:'Duration of the semester',
+        text:'There shall be 2 semsters (Fall and Spring) of 18 weeks each in an academic year. The commencement of semester shall be regulates by the Academic Council. (Explanaiton: Out fo 18 weeks, 16 weeks shall be actual teaching time, the rest may be utilized for ebrikknebtm conduct of examninatiuons and declaration of results,etc.)'
+    },
+    {
+        heading:'Credit houes requirments for various degrees',
+        text:'The minimum credit hours requirements for Bs degree programs hsall as foolows; BS(4 years) 124 credit hours'
+    },
+    {
+        heading:'Routing of Academic Matters',
+        text:'All the academic mattes hsall be routed through proper channel i.e. through the chaiman/Incharge of the Deparment/Director and Principal/Dean/Coordinator of the faculty concerned.'
+    },
+    {
+        heading:'Time for Admission',
+        text:'Admission shall be conducted in the Fall Semster and only once in a calendar year.If a candidate fails to complete enrollment, his/her admission hsall be concelled.',
+    },
+    {
+        heading:'Merit of Admission',
+        text:'The admission to bachelor degree Bs(FYDP) programs shall be made on the basids of merit.',
+        Lists:[
+            'Lists of selected candidates shall be made on the basix of merit',
+            'The competent authority may refuse admission to a candidate without assigning any reason.'
+        ]
+    },
+    {
+        heading:'Discontinuations/Freeeze of Study/Semester',
+        Lists:[
+            'A student may dis-continue enrolled semster before appearing in the final examination with the premission of the Vice Chancellor obtained through the Principal on the recommendations of the HOD/incharge, on account of sickness duly certified by the Medical Officer or dur to circumstances beyond his/her control subject to fulfilment of condition that the studnet has passed the final examination of pervious smester with minimum prescried GPA/CGPA required for the said degree program.',
+            'The facility of discontination can be availed only once during whole degree program and it could be for two smester only.',
+            'A studnent so premitted to discontinue is required to resume his/her studies from the next smester on the reocmmendation of Director/Principal/ Chairman concrened and notified by the Dean/Coordinator,',
+            'During the semester freeze, bonafide status of the student shall  remain suspended. The studen wil not be entitiled to avail anyu privilege as that of a regular student.'
+        ]
+    },
+    {
+        heading:'Re-Admission',
+        Lists:[
+            'If a student fails ot enroll in any semster(s) without permission of the competent authority, the student shall cease to be on the rools of the University and in case the studnet desires re-admission, the studeent shall have to apply for the same.',
+            'The HOD/Incharge may refuse the re-admission if the reasons presented are not convincing.'
+        ]
+    },
+    {
+        heading:'Couse of Study',
+        Lists:[
+            'Allgraduate courses will be desigbated appropriate course code nu bers and will be preceded by the suitable alphabets indicating the name of the respective deparment.',
+            'The Teachers will be required to hand over detailed course outline to the student within 7 days of hte begining of semester and will send a copy of course outline and work plan to the chaiman of the concrened department Director Academic.',
+            'A full time student can enroll up to 18 credit hours in a regular smester.',
+            'A course can be repeated two times (maximum three times enrollment) at the most. The controller of Examination office will mention corresponding repetitions in the result notificaiton and the student shall methon repetion status in the Course Registrations Form',
+            'The Principal may consider credits earned by a student at another recognized institution, subject to a maximum of 50 percent of the minimum credits requirements of the degree, on the recommendations of Equialence Committee provided that: The contents of course (s) for which credit is claimed, are identical or similar to the course included in his/her planned course work and the course for which credit is claimed have not been used for any other and higher degree.',
+            'A course studied to qualify a degree will not be taken/considered for any other and higher degreee program.',
+            'Credit earned for a course shall lapse on the expiry of five years for regular students and seven years for part time students from the end of the semster in which the course was qualified. The Dean/Coordinator may re-evaluate the lapsed course for special resons to be recorded.',
+            'No course shall carry more than 4 credits.'
+        ]
+    },
+    {
+        heading:'Examinaitons',
+        Lists:[
+            'Tests and Examinations:',
+            'There shall be two examinations mid and final in each semster. In addition to these examinations the teacher shall give home assignments and quizzes etc. to the students.The mode of these examinations will be left to the teacher who will be solely responsible for the conduct of examination as well as evaluation in his/hewr course. The grade given in the course by the teacher shall be final.',
+            'Only those studnets, who have at least 75% attendance in theory and practical (where applicable) separately in each course, shall be eligible to appear in the final examination.',
+            'A teacher shall report to the Head of the resective department, the names of studnets who are absent form the lectures/ practicals (where applicable) continusly for seven days to enavle the office of the Principal to struck off names of such students. The studnet may be readmitted under the college rules or may be refused admission if the  reason provided for readmission are not convincing.',
+            'Final Examination: The final examination for a semster shall be held on a date and time and place to be notified by the Controller of Examinations.',
+            'The scripts of each examination shall be discussed with the students.',
+            'The mid-semester examination shall be held during 9th week of the semster which shall carry 20% of the total allocated marks for the course.This examination shall be held by the teacher concerned who shall proceed accoring to the College rules.',
+            'The Following weightage shall be given to the examinations, home assignments,etc.',
+            'A Mid-semster Examicaiotn                                20%',
+            'Home assignments/quizzes, presentations etc.             10%',
+            'Final Examination                                        70%',
+            'Total                                                    100%',
+            'To qualify a course, it is essentialto pass separately in the theory and practical (where applicable) examinations with aggregation to be at least 40%.',
+            'The teacher shallsend the final award list along with answer sheets of mid Exam and assignment evalution list of the course to the office of the Controller of Examination.'
+
+        ]
+    },
+    {
+        heading:'Grade point Average',
+        Lists:[
+            'Greade point and equivalence between letter grading and numercial grading shall be as folows:',
+            'If a studnet fails to obtian CGPA of 1.75 at the end of each academic year (Fall and spring) his/her admission shall stand cancelled. However the student may seek fresh admission.',
+            'A studnet, who obtains CGPA of 1.75 or more but less than 2.50 upon the completion of entire approved course work, may be allowed to repeat once the courses fo the previous semster in which the student had obtained the lowest grades in order to imporove the CGPA so as to obtian the minimum of 2.50, failing which the student shall cease to be on the rools provided that he/she has availed the maximum premissible repetions.',
+            'CGPA of 2.5 out of 4.0 is required to quality for the award of degree.'
+        ]
+    },
+    {
+        heading:'Thesis / Research report',
+        Lists:[
+            'A student can opt for thesis in lieu of two optional courses of 3 credit hours each.',
+            'Student with minimum 3.00 CGPA shall be eligible for opting the thesis option.',
+            'A studnt shall be required to submit a project research report and present in in a seminar for evalution conducted by an axternal examiner. The student ahs to clear all the courses before appearing in the final seminar/via of the research project.',
+            'There shall be 120 marks corresponding to 6 credit hour (equivalent to 2 optional courses of minimum 6 credit hours) allocated for thesis and these may be divided as under: Total marks so awarded will be converted into a letter grade and average gradesof the baiss of course work and project reporyt will then be worked to calculate the final CGPA',
+            'The supervisor shall send he award list of thesis in sealed envelope to the office of the Controller of Examinations shall notify the final result fo thte student.'
+
+        ]
+    },
+    {
+        heading:'Supervisory Committee',
+        Lists:[
+            'The superviosry committee shall be proposed by the respective Board of studies of the Department during 1st semster.',
+            'Supervisory Committee shall consist of 03 teachers of the department. However, if an outstanding specialist in a major or minor/elective feild of studey is availble outside the university, he/she may be taken as a member of the supervisory Committee in place of teacher or as an additional member.',
+            'Supervisory of students will act as chaimrn of the supervisory committee.',
+            'Superviosry commitee proposed by the respctive Board of Studies and recommeded by the chairman/ Director of the Institute/Principal, shall be approved by the competent authoity.',
+            'Supervisory committee may be revised accoring to the regulation with reaons of changed be recorded.',
+            'In case, a student duly recommended by the HOD/Incharge concerned to conduct this/her complete/ part of thesis research in other instituion/ laboratory, is allowed by the office of the Principal. The helping scientist may be taken as co-supervior/ special member on the supervisory committee of the student. However, the College will not bear an financial liability in this context.',
+            'Foreign Faculry member, HEC Naitonal Professor/ HEC Eminetn Scientist may act as Superivisor/ member of the supervisory committee providec that they are appointed for three years.',
+            'Adhoc/contact teacher may act as supervior/member of the supervioory committee only where there is shortage of regular faculry member.'
+        ]
+    },
+    {
+        heading:'General',
+        Lists:[
+            'Office of the HOD/Coordinator will maintain student record for 3 years after the declaraiton of final result. After that period, the recordmay be disposed off with the permission of the Principal.',
+            'Office of the controller ofExamination shall keep scripts of students examinations for two semsters (one year). After period, the record may be disposed off with the permisson of the Principal.'
+        ]
+    }
+]
